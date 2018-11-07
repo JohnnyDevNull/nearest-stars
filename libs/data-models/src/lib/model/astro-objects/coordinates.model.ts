4b10f9b1fb42c1d413eaddbd7773ccf6cefb49f3
JohnnyDevNull <philipp.john@live.de>
2018-11-08 00:32:41 +0100

    models doc

--- libs/data-models/src/lib/model/astro-objects/coordinates.model.ts
+++ libs/data-models/src/lib/model/astro-objects/coordinates.model.ts
@@ -1,7 +1,35 @@
 export interface CoordinatesModel {
+
+  /**
+   * The identifier.id the data belongs to.
+   */
   objectId?: number;
+
+  /**
+   * equatorial coordinates of the star
+   *
+   * unit/format: degrees
+   */
   ra?: number;
+
+  /**
+   * error in the previous positions
+   *
+   * unit/format: arcsec
+   */
   raErr?: number;
+
+  /**
+   * equatorial coordinates of the star
+   *
+   * unit/format: degrees
+   */
   dec?: number;
+
+  /**
+   * error in the previous positions
+   *
+   * unit/format: arcsec
+   */
   decErr?: number;
 }
--- libs/data-models/src/lib/model/astro-objects/identifier.model.ts
+++ libs/data-models/src/lib/model/astro-objects/identifier.model.ts
@@ -1,6 +1,31 @@
 export interface IdentifierModel {
+
+  /**
+   * The database internal identifier:
+   * Possible Format: [A-Z0-9]
+   */
   id?: string;
+
+  /**
+   * The named identifier of the object.
+   * Primary name by which the object is known.
+   */
   name?: string;
+
+  /**
+   * Star system the object belongs to.
+   * Proxima Centauri would be in the system Alpha Centauri
+   */
   system?: string;
+
+  /**
+   * Distribution of stars in the system, if multiple.
+   * Example, for quintuple system V1054 Ophiuchi
+   * ((A(BC))D)E where stars BC orbit each other
+   * and stars BC are collectively orbited by star A
+   * Stars A,B, and C are orbited by star D
+   * and stars ABC and D are all orbited by star E.
+   */
   hierarchy?: string;
+
 }
