e6082eb1d3428d113814f78f6ec4efdc53495042
JohnnyDevNull <philipp.john@live.de>
2018-11-08 20:26:02 +0100

    Login, Logout & Signup pages

--- apps/client/src/app/app-routing.module.ts
+++ apps/client/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './comps/main/main.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent, children: [
     { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'signup', component: SignupComponent },
     { path: '**', redirectTo: 'home' }
   ] }
 ];
--- apps/client/src/app/pages/pages.module.ts
+++ apps/client/src/app/pages/pages.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    SignupComponent
   ],
   exports: [
     HomeComponent
