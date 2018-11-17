b2e31bab2b18371af76800137ef1abf217511f67
JohnnyDevNull <philipp.john@live.de>
2018-11-17 22:06:36 +0100

    add version output to index ctrl

--- apps/astro-api/src/controllers/index-controller.ts
+++ apps/astro-api/src/controllers/index-controller.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from 'express';
 import { BaseRestModel } from '@nearest-stars/data-models';
+import { Request, Response } from 'express';
+import { environment } from '../environments/environment';
 
 export class IndexController {
   public getIndex (req: Request, res: Response) {
     const result: BaseRestModel<any> = {
       meta: {
         code: 0,
-        message: 'The api is online!',
+        message: 'The api is online! Version: ' + environment.version,
       }
     };
     res.json(result);
