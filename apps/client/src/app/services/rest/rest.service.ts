2f83c76431f4033f1712f64f5c78b19d6794b00f
JohnnyDevNull <philipp.john@live.de>
2018-11-25 22:29:47 +0100

    impl client base rest service

---
+++ apps/client/src/app/services/rest/rest.service.ts
@@ -0,0 +1,62 @@
+import { BaseRestModel } from '@nearest-stars/data-models';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { StateService } from '../state/state-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+  public constructor(
+    private http: HttpClient,
+    private state: StateService
+  ) {
+  }
+
+  public doGet<T>(path: string, needsAuth = false) {
+    const httpOptions = this.httpOptions;
+    if (needsAuth) {
+      httpOptions.headers = httpOptions.headers.append(
+        'Authorization', 'Bearer ' + this.state.getToken()
+      );
+    }
+    return this.http.get<BaseRestModel<T>>(path, httpOptions);
+  }
+
+  public doPost<T>(path: string, options: any, needsAuth = false) {
+    const httpOptions = this.httpOptions;
+    if (needsAuth) {
+      httpOptions.headers = httpOptions.headers.append(
+        'Authorization', 'Bearer ' + this.state.getToken()
+      );
+    }
+    return this.http.post<T>(path, options, this.httpOptions);
+  }
+
+  public doPut<T>(path: string, options: any, needsAuth = false) {
+    const httpOptions = this.httpOptions;
+    if (needsAuth) {
+      httpOptions.headers = httpOptions.headers.append(
+        'Authorization', 'Bearer ' + this.state.getToken()
+      );
+    }
+    return this.http.put<T>(path, options);
+  }
+
+  public doDelete<T>(path: string, options: any, needsAuth = false) {
+    const httpOptions = this.httpOptions;
+    if (needsAuth) {
+      httpOptions.headers = httpOptions.headers.append(
+        'Authorization', 'Bearer ' + this.state.getToken()
+      );
+    }
+    return this.http.delete<T>(path, options);
+  }
+}
