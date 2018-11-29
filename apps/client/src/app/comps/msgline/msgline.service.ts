eff74ecb4416962e78cbe74b3a27de64bd774692
JohnnyDevNull <philipp.john@live.de>
2018-11-29 23:16:23 +0100

    msgline add showMessageByResult

--- apps/client/src/app/comps/msgline/msgline.service.ts
+++ apps/client/src/app/comps/msgline/msgline.service.ts
@@ -31,4 +31,34 @@ export class MsglineService {
     this.messages.splice(this.messages.indexOf(msg), 1);
     this.updSubj.next(1);
   }
+
+  public showMessageByResult(result: any): void {
+    console.log(result);
+    let text: string;
+    let type: string;
+
+    if (result !== undefined && result.meta !== undefined) {
+      if (result.meta.code === 0) {
+        type = MsglineTypeEnum.SUCCESS;
+      } else if (result.meta.error) {
+        type = MsglineTypeEnum.DANGER;
+      } else {
+        type = MsglineTypeEnum.WARNING;
+      }
+      text = result.meta.message;
+    } else if (result.error !== undefined) {
+      type = MsglineTypeEnum.DANGER;
+      if (typeof result.error === 'string') {
+        text = result.error;
+      } else if (result.error.meta !== 'undefined') {
+        text = result.error.meta.message;
+      }
+    } else if (result.message !== undefined ) {
+      type = MsglineTypeEnum.WARNING;
+      text = result.message;
+    }
+
+    this.messages.push({text, type});
+    this.updSubj.next(1);
+  }
 }
